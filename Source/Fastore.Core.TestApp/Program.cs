97ca5ca6c402cce1aeac310b4f225f2a807c820f
jpercival <jpercival@b30075cd-70d6-584e-9a73-6433d0c45435>
2012-01-20 22:08:35 +0000

    TestApp
    git-svn-id: https://svn.alphora.com:8443/svn/Fastore/trunk@42 b30075cd-70d6-584e-9a73-6433d0c45435

---
+++ Source/Fastore.Core.TestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Fastore.Core.Test;
+
+namespace Fastore.Core.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new MemoryUsageTests();
+
+            test.TestMethod2();
+        }
+    }
+}
