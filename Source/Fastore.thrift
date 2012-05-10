fa3d69fd4a0630c41652f5f9c1e016685c3e0ec6
n8allan <n8allan@b30075cd-70d6-584e-9a73-6433d0c45435>
2012-05-10 07:59:03 +0000

    Initial completion of the IDL.
    git-svn-id: https://svn.alphora.com:8443/svn/Fastore/trunk@212 b30075cd-70d6-584e-9a73-6433d0c45435

--- Source/Fastore.thrift
+++ Source/Fastore.thrift
@@ -1,78 +1,111 @@
-namespace cpp fastore;
-namepsace cs Alphora.Fastore;
+namespace cpp fastore
+namespace csharp Alphora.Fastore
 
 // Common
 
-typedef i64 Revision;
+typedef i64 Revision
 
 // Topology
 
-typedef i32 TopologyID;
-typedef i32 ColumnID;
-typedef i32 HostID;
-typedef string HostAddress;
-
-struct Column
-{
-	1: ColumnID ID,
-	2: string Name
-	3: bool IsUnique,
-	4: string Type,
-	5: string IDType
-}
+typedef i32 TopologyID
+typedef i32 ColumnID
+typedef i32 HostID
+typedef string HostAddress
 
 struct Host
 {
 	1: HostID ID,
 
 	/** Host name and optional port (e.g. "myserver:1234") */
-	2: HostAddress Address
+	2: HostAddress Address,
 }
 
-struct ColumnHost
+struct Repository
 {
 	1: ColumnID ColumnID,
-	2: HostID HostID
+	2: HostID HostID,
 }
 
 struct Topology
 {
 	1: TopologyID ID,
-	2: set<Column> Columns,
-	4: set<Host> Hosts,
-	5: set<ColumnHost> ColumnHosts,
-	6: Revision Revision = 1
+	2: set<Host> Hosts,
+	3: set<Repository> Repositories
+}
+
+struct TopologyResult
+{
+	1: Topology Topology,
+	2: Revision Revision
+}
+
+// Topology Reporting
+
+enum RepositoryStatus
+{
+	/** Not in-sync; don't send read or write requests. */
+	Loading = 1,
+	/** In the process of unloading; don't send any requests. */
+	Unloading = 2,
+	/** Online and servicing requests */
+	Online = 3,
+	/** In the process of checkpointing; can send read and write requests, but performance may be sub-optimal. */
+	Checkpointing = 4
+}
+
+enum HostStatus
+{
+	Offline = 1,
+	Online = 2,
+	Unreachable = 3
+}
+
+struct HostReport
+{
+	1: HostStatus Status,
+	2: map<ColumnID, RepositoryStatus> RepositoryStatus
+}
+
+struct TopologyReport
+{
+	1: TopologyID TopologyID,
+	2: map<HostID, HostReport> Hosts,
+	3: Revision Revision = 1
 }
 
 // Host
 
-typedef i64 LockID;
-typedef string LockName;
-enum LockMode { Read = 1, Write = 2 };
-/** Timeout in ms; -1 = infinite; 0 = fail immediately */
-typedef i32 LockTimeout;
+typedef i64 LockID
+typedef string LockName
+enum LockMode { Read = 1, Write = 2 }
+/** Timeout in ms; <= 0 means fail immediately. */
+typedef i32 LockTimeout
+const LockTimeout DefaultLockTimeout = 1000;
 
-typedef i64 TransactionID;
+struct TransactionID
+{
+	1: Revision Revision,
+	2: i64 Key
+}
 
 struct Include
 {
-	1: ColumnID ColumnID,
-	2: binary RowID,
-	3: binary Value
+	1: binary RowID,
+	2: binary Value
 }
 
 struct Exclude
 {
-	1: ColumnID ColumnID,
-	2: binary RowID
+	1: binary RowID
 }
  
-struct Changes
+struct ColumnWrites
 {
-	1: TransactionID TransactionID,
-	2: set<Include> Includes,
-	3: set<Exclude> Excludes
+	1: set<Include> Includes,
+	2: set<Exclude> Excludes
 }
+
+typedef map<ColumnID, ColumnWrites> Writes
 
 struct Statistic
 {
@@ -89,36 +122,10 @@ struct RangeBound
 
 struct RangeRequest
 {
-	1: ColumnID ColumnID, 
-	2: i32 Limit = 500,
-	3: bool Ascending,
-	4: optional RangeBound Start,
-	5: optional RangeBound End
-}
-
-struct RangeResponse
-{
-	1: list<ValueRows> ValueRows,
-	2: Revision Revision
-}
-
-struct ValueRequest
-{
-	1: ColumnID ColumnID,
-	2: list<binary> RowIDs
-}
-
-struct ValueResponse
-{
-	1: list<binary> Values,
-	2: Revision Revision
-}
-
-struct OrderRequest
-{
-	1: ColumnID ColumnID,
-	2: list<list<binary>> RowIDLists,
-	3: bool Ascending
+	1: i32 Limit = 500,
+	2: bool Ascending = true,
+	3: optional RangeBound Start,
+	4: optional RangeBound End
 }
 
 struct ValueRows
@@ -127,33 +134,127 @@ struct ValueRows
 	2: list<binary> RowIDs
 }
 
-struct OrderResponse
+typedef list<ValueRows> ValueRowsList
+
+struct Query
 {
-	1: list<ValueRows> ValueRowsList,
-	2: Revision Revision
+	1: list<binary> RowIDs,
+	2: list<RangeRequest> Ranges
 }
+
+typedef map<ColumnID, Query> Queries
+
+struct Answer
+{
+	1: list<binary> RowIDValues,
+	2: list<ValueRowsList> RangeValues
+}
+
+struct ReadResults
+{
+	1:map<ColumnID, Answer> Answers,
+	2:Revision Revision
+}
+
+typedef map<Query, Answer> Read
+
+typedef map<ColumnID, Read> Reads
+	
+
+exception NotLatest
+{
+	1: Revision Latest
+}
+
+exception Conflict
+{
+	1: string Details
+}
+
+exception BeyondHistory
+{
+	1: Revision MinHistory
+}
+
+exception LockExpired
+{
+	1: LockID LockID
+}
+
+exception LockTimedOut {}
 
 service Host
 {
-	1: Topology GetTopology(),
+	/** Returns the target topology as this host presently understands it. */
+	TopologyResult GetTopology(),
 
-	2: TransactionID StartTransaction(),
-	3: void PrepareTransaction(1:TransactionID transactionID),
-	4: Revision CommitTransaction(1:TransactionID transactionID),
-	5: void WaitTransaction(1:TransactionID transactionID),
+	/** Updates the topology and returns the new topology revision - GRID COORDINATED. */
+	Revision PrepareTopology(1:TransactionID transactionID, 2:Topology topology),
 
-	6: LockID GetLock(1:LockName name, 2:LockMode mode, 3:LockTimeout timeout = -1),
-	7: void EscelateLock(1:LockID lockID, 2:LockTimeout timeout = -1),
-	8: void ReleaseLock(1:LockID lockID),
+	/** Informs that the prepare was successful, the change should be committed. */
+	void CommitTopology(1:TransactionID transactionID),
 
-	9: list<RangeResponse> GetRanges(1:list<RangeRequest> requests),
-	10: list<ValueResponse> GetValues(1:list<ValueRequest> requests),
-	11: list<OrderResponse> GetOrdered(1:list<OrderRequest> requests),
+	/** Informs that the prepare was unsuccessful, the change should be rolled back. */
+	void RollbackTopology(1:TransactionID transactionID),
 
-	12: void Apply(1:Changes changes),
+	/** Returns the current status of all hosts as this host understands it. */
+	TopologyReport GetTopologyReport(),
+
+	/** Returns the current status of this host. */
+	HostReport GetReport(),
+
+
+	/** Validates that the transaction ID is updated to the latest and then Applies all changes - GRID COORDINATED. */
+	Revision Prepare(1:TransactionID transactionID, 2:Writes writes, 3:Reads reads) 
+		throws (1:NotLatest notLatest),
 	
-	13: list<Statistic> GetStatistics(1:list<ColumnID> columnIDs); 
+	/** Applies the given writes as of the latest revision (regardless of whether the transaction ID is out of date), 
+		returns an updated Transaction ID - GRID COORDINATED. */
+	TransactionID Apply(1:TransactionID transactionID, 2:Writes writes),
 
-	// TODO: Revisor logic 
+	/** Informs that the prepare was successful, the changes should be committed. */
+	void Commit(1:TransactionID transactionID),
+
+	/** Informs that the prepare was unsuccessful, the changes should be rolled back. */
+	void Rollback(1:TransactionID transactionID),
+
+	/** Waits for the given transaction to be flushed to disk */
+	void Flush(1:TransactionID transactionID),
+
+
+	/** Determines whether the given set of reads conflict with any intervening revisions. */
+	bool DoesConflict(1:Reads reads, 2:Revision source, 3:Revision target)
+		throws (1:BeyondHistory beyondHistory),
+
+	/** Updates the given transaction to the latest by validating reads and writes for conflicts, and returns a new TransactionID. */
+	TransactionID Update(1:TransactionID transactionID, 2:Writes writes, 3:Reads reads)
+		throws (1:Conflict conflict),
+	
+	/** Upgrades or downgrades the given reads to match the data as of a given revision. */
+	Reads Transgrade(1:Reads reads, 2:Revision source, 3:Revision target)
+		throws (1:BeyondHistory beyondHistory),
+
+	
+	/** Acquires a given named lock given a mode and timeout. */
+	LockID AcquireLock(1:LockName name, 2:LockMode mode, 3:LockTimeout timeout = DefaultLockTimeout)
+		throws (1:LockTimedOut timeout),
+
+	/** Keeps a given lock alive - locks automatically expire if not renewed. */
+	void KeepLock(1:LockID lockID)
+		throws (1:LockExpired expired),
+	
+	/** Attempts to escalate the given lock to write mode */
+	void EscalateLock(1:LockID lockID, 2:LockTimeout timeout = -1)
+		throws (1:LockTimedOut timeout, 2:LockExpired expired),
+
+	/** Releases the given lock */
+	void ReleaseLock(1:LockID lockID)
+		throws (1:LockExpired expired),
+
+	/** Retrieves data and the latest revision number corresponding to a given list of queries. */
+	ReadResults Query(1:Queries queries),
+	
+	/** Retrieves statistics for a given list of columns based on the latest committed revision. */
+	list<Statistic> GetStatistics(1:list<ColumnID> columnIDs)
 }
 
