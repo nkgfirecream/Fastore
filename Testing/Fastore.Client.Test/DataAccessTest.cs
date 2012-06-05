d4c25ac9f71976c1d53109a2f5cd73f3002e0b0d
mpetersen <mpetersen@b30075cd-70d6-584e-9a73-6433d0c45435>
2012-06-05 22:04:18 +0000

    git-svn-id: https://svn.alphora.com:8443/svn/Fastore/trunk@282 b30075cd-70d6-584e-9a73-6433d0c45435

--- Testing/Fastore.Client.Test/DataAccessTest.cs
+++ Testing/Fastore.Client.Test/DataAccessTest.cs
@@ -8,38 +8,8 @@ using Alphora.Fastore.Client;
 namespace Fastore.Client.Test
 {
     [TestClass]
-    public class QueryTest
+    public class DataAccessTest : TestSetup
     {
-        private Database _database;
-        private int[] _columns;
-
-        public void createTable(int[] cols)
-        {
-            string hostname = "localhost";
-            const int port = 8064;
-            _database = Alphora.Fastore.Client.Client.Connect(hostname, port);
-
-            int[] _schemaColumns = new int[] { 0, 1, 2, 3, 4 };
-            
-            _database.Include(_schemaColumns, cols[0], new object[] { cols[0], "ID", "Int", "Int", true });
-            _database.Include(_schemaColumns, cols[1], new object[] { cols[1], "Given", "String", "Int", false });
-            _database.Include(_schemaColumns, cols[2], new object[] { cols[2], "Surname", "String", "Int", false });
-            _database.Include(_schemaColumns, cols[3], new object[] { cols[3], "Gender", "Bool", "Int", false });
-            _database.Include(_schemaColumns, cols[4], new object[] { cols[4], "BirthDate", "String", "Int", false });
-            _database.Include(_schemaColumns, cols[5], new object[] { cols[5], "BirthPlace", "String", "Int", false });
-        }
-
-        public void addData(Database db, int[] cols)
-        {
-            db.Include(cols, 1, new object[] { 1, "Joe", "Shmoe", true, "5/26/1980", "Antarctica" });
-            db.Include(cols, 2, new object[] { 2, "Ann", "Shmoe", false, "4/20/1981", "Denver" });
-            db.Include(cols, 3, new object[] { 3, "Sarah", "Silverman", false, "11/10/1976", "Chicago" });
-            db.Include(cols, 4, new object[] { 4, "Bob", "Newhart", true, "12/2/1970", "Paris" });
-            db.Include(cols, 5, new object[] { 5, "Samantha", "Smith", false, "1/13/1984", "Tokyo" });
-            db.Include(cols, 6, new object[] { 6, "Andy", "Warhol", true, "9/14/1987", "New York" });
-            db.Include(cols, 7, new object[] { 7, "Carl", "Sagan", true, "4/1/1957", "Las Vegas" });
-        }
-
         [TestMethod]
         public void TestCreateTable(){
         string hostname1 = "localhost";
---
+++ Testing/Fastore.Client.Test/TransactionTest.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Alphora.Fastore.Client;
+
+namespace Fastore.Client.Test
+{
+    [TestClass]
+    public class TransactionTest : TestSetup
+    {
+        [TestMethod]
+        public void TestTransaction()
+        {
+            _columns = new int[] { 1000, 1001, 1002, 1003, 1004, 1005 };
+            createTable(_columns);
+            addData(_database, _columns);
+
+            Transaction _transaction = _database.Begin(false, false);
+
+            _database.Include(_columns, 8, new object[] { 8, "Scott", "Pilgrim", true, "10/13/1979", "Moscow" });
+            var data = _database.GetRange(
+                _columns,
+                new[] { new Order { ColumnID = 1000, Ascending = true } },
+                new[] { new Range { ColumnID = 1000, Limit = 8 } }
+                );
+
+            var tData = _transaction.GetRange(
+                _columns,
+                new[] { new Order { ColumnID = 1000, Ascending = true } },
+                new[] { new Range { ColumnID = 1000, Limit = 8 } }
+                );
+            Assert.AreEqual(data.Count, 8);
+            Assert.AreEqual(tData.Count, 8);
+
+            _database.Exclude(_columns, 8);
+            data = _database.GetRange(
+              _columns,
+              new[] { new Order { ColumnID = 1000, Ascending = true } },
+              new[] { new Range { ColumnID = 1000, Limit = 8 } }
+              );
+
+            tData = _transaction.GetRange(
+                _columns,
+                new[] { new Order { ColumnID = 1000, Ascending = true } },
+                new[] { new Range { ColumnID = 1000, Limit = 8 } }
+                );
+            Assert.AreEqual(data.Count, 7);
+            Assert.AreEqual(tData.Count, 7);
+
+        }
+
+        [TestMethod]
+        public void TestTransactionCommit()
+        {
+            _columns = new int[] { 1000, 1001, 1002, 1003, 1004, 1005 };
+            createTable(_columns);
+            addData(_database, _columns);
+
+            Transaction _transaction = _database.Begin(false, false);
+
+            var data = _database.GetRange(
+                _columns,
+                new[] { new Order { ColumnID = 1000, Ascending = true } },
+                new[] { new Range { ColumnID = 1000, Limit = 8 } }
+                );
+
+            var tData = _transaction.GetRange(
+                _columns,
+                new[] { new Order { ColumnID = 1000, Ascending = true } },
+                new[] { new Range { ColumnID = 1000, Limit = 8 } }
+                );
+            Assert.AreEqual(data.Count, 7);
+            Assert.AreEqual(tData.Count, 7);
+
+            _transaction.Include(_columns, 8, new object[] { 8, "Martha", "Stewart", false, "4/10/1967", "San Jose" });
+            data = _database.GetRange(
+                _columns,
+                new[] { new Order { ColumnID = 1000, Ascending = true } },
+                new[] { new Range { ColumnID = 1000, Limit = 8 } }
+                );
+            tData = _transaction.GetRange(
+                _columns,
+                new[] { new Order { ColumnID = 1000, Ascending = true } },
+                new[] { new Range { ColumnID = 1000, Limit = 8 } }
+                );
+            Assert.AreEqual(data.Count, 7);
+            Assert.AreEqual(tData.Count, 8);
+
+            _transaction.Commit();
+            data = _database.GetRange(
+                _columns,
+                new[] { new Order { ColumnID = 1000, Ascending = true } },
+                new[] { new Range { ColumnID = 1000, Limit = 8 } }
+                );
+            tData = _transaction.GetRange(
+                _columns,
+                new[] { new Order { ColumnID = 1000, Ascending = true } },
+                new[] { new Range { ColumnID = 1000, Limit = 8 } }
+                );
+            Assert.AreEqual(data.Count, 8);
+            Assert.AreEqual(tData.Count, 8);
+
+            _transaction.Exclude(_columns, 8);
+            data = _database.GetRange(
+                _columns,
+                new[] { new Order { ColumnID = 1000, Ascending = true } },
+                new[] { new Range { ColumnID = 1000, Limit = 8 } }
+                );
+            tData = _transaction.GetRange(
+                _columns,
+                new[] { new Order { ColumnID = 1000, Ascending = true } },
+                new[] { new Range { ColumnID = 1000, Limit = 8 } }
+                );
+            Assert.AreEqual(data.Count, 8);
+            Assert.AreEqual(tData.Count, 7);
+
+            _transaction.Commit();
+            data = _database.GetRange(
+                _columns,
+                new[] { new Order { ColumnID = 1000, Ascending = true } },
+                new[] { new Range { ColumnID = 1000, Limit = 8 } }
+                );
+            tData = _transaction.GetRange(
+                _columns,
+                new[] { new Order { ColumnID = 1000, Ascending = true } },
+                new[] { new Range { ColumnID = 1000, Limit = 8 } }
+                );
+            Assert.AreEqual(data.Count, 7);
+            Assert.AreEqual(tData.Count, 7);
+        }
+
+        [TestMethod]
+        public void TestTransactionConflict()
+        {
+            _columns = new int[] { 1000, 1001, 1002, 1003, 1004, 1005 };
+            createTable(_columns);
+            addData(_database, _columns);
+
+            Transaction _transaction = _database.Begin(false, false);
+
+            _transaction.Include(_columns, 8, new object[] { 8, "Scott", "Pilgrim", true, "10/13/1979", "Moscow" });
+            _database.Include(_columns, 8, new object[] { 8, "Martha", "Stewart", false, "4/10/1967", "San Jose" });
+            bool flag = false;
+            try
+            {
+                _transaction.Commit();
+            }
+            catch
+            {
+                flag = true;
+            }
+            Assert.AreEqual(flag, true);
+        }
+
+        [TestMethod]
+        public void TestDispose()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestRollback()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestEnsureColumnLog()
+        {
+
+        }
+    }
+}
