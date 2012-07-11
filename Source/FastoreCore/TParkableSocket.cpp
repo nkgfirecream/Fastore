f1992d9b0634bcb2c8e0cda22943a1462a080de9
jpercival <jpercival@b30075cd-70d6-584e-9a73-6433d0c45435>
2012-07-11 23:24:39 +0000

    Beginnings of parkable socket.
    git-svn-id: https://svn.alphora.com:8443/svn/Fastore/trunk@380 b30075cd-70d6-584e-9a73-6433d0c45435

---
+++ Source/FastoreCore/TParkableSocket.cpp
@@ -0,0 +1,18 @@
+#include "TParkableSocket.h"
+
+TParkableSocket::TParkableSocket(int socket)
+	: TSocket(socket) { }
+
+TParkableSocket::TParkableSocket(std::string host, int port)
+	: TSocket(host, port) { }
+
+TParkableSocket::TParkableSocket(std::string path)
+	: TSocket(path) { }
+
+TParkableSocket::TParkableSocket()
+	: TSocket() { }
+
+void TParkableSocket::setParkCallback(std::function<void(int)> callback)
+{
+	_callback = callback;
+}
---
+++ Source/FastoreCore/TParkableSocket.h
@@ -0,0 +1,23 @@
+//Might not be needed.. Still exploring..
+
+#include <thrift/transport/TSocket.h>
+#include <functional>
+
+using namespace apache::thrift::transport;
+
+class TParkableSocket : public TSocket
+{
+	public:
+	
+	TParkableSocket(int socket); 
+	TParkableSocket(std::string path);
+	TParkableSocket(std::string host, int port);
+	TParkableSocket();
+
+	void resume();
+	void setParkCallback(std::function<void(int)> callback);
+
+	private:
+	
+	std::function<void(int)> _callback;
+};
