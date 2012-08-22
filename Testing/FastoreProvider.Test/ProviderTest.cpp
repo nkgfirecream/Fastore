8ae3f8266f3b09281cff6bcbc57cb5fe4899fbb8
mpetersen <mpetersen@b30075cd-70d6-584e-9a73-6433d0c45435>
2012-08-22 21:50:54 +0000

    git-svn-id: https://svn.alphora.com:8443/svn/Fastore/trunk@470 b30075cd-70d6-584e-9a73-6433d0c45435

---
+++ Testing/FastoreProvider.Test/ProviderTest.cpp
@@ -0,0 +1,59 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "..\FastoreProvider\fastore.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace FastoreProviderTest
+{		
+	TEST_CLASS(ProviderTest)
+	{
+	public:
+		
+		TEST_METHOD(TestConnect)
+		{
+			// TODO: Your test code here
+		}
+
+		TEST_METHOD(TestDisconnect)
+		{
+			// TODO: Your test code here
+		}
+
+		TEST_METHOD(TestPrepare)
+		{
+			// TODO: Your test code here
+		}
+
+		TEST_METHOD(TestBind)
+		{
+			// TODO: Your test code here
+		}
+
+		TEST_METHOD(TestNext)
+		{
+			// TODO: Your test code here
+		}
+
+		TEST_METHOD(TestColumnInfo)
+		{
+			// TODO: Your test code here
+		}
+
+		TEST_METHOD(TestColumnValue)
+		{
+			// TODO: Your test code here
+		}
+
+		TEST_METHOD(TestClose)
+		{
+			// TODO: Your test code here
+		}
+
+		TEST_METHOD(TestExecute)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
