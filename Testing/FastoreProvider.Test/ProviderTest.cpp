d24c4da627ba38c5044a36b37404e22db7388d47
mpetersen <mpetersen@b30075cd-70d6-584e-9a73-6433d0c45435>
2012-08-22 21:58:03 +0000

    git-svn-id: https://svn.alphora.com:8443/svn/Fastore/trunk@471 b30075cd-70d6-584e-9a73-6433d0c45435

--- Testing/FastoreProvider.Test/ProviderTest.cpp
+++ Testing/FastoreProvider.Test/ProviderTest.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
-#include "..\FastoreProvider\fastore.h"
+#include "..\..\Source\FastoreProvider\fastore.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
