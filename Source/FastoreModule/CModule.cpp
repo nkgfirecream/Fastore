0c40e8cf273b9bba17ae0965020c1af1c3fa2292
jpercival <jpercival@b30075cd-70d6-584e-9a73-6433d0c45435>
2012-09-05 23:56:04 +0000

    C wrapper for FastoreModule call for FastoreShell
    git-svn-id: https://svn.alphora.com:8443/svn/Fastore/trunk@517 b30075cd-70d6-584e-9a73-6433d0c45435

--- Source/FastoreModule/CModule.cpp
+++ Source/FastoreModule/CModule.cpp
@@ -5,4 +5,18 @@
 void intializeFastoreModule(sqlite3* db, int argc, void* argv)
 {
 	//Convert from c to cpp...
+	std::vector<module::Address> mas;
+	module::Address address;
+
+	string text((char*)argv);
+	istringstream reader(text, istringstream::in);
+	std::getline(reader, address.Name, ';');
+
+	string numtext;
+	std::getline(reader, numtext);
+	
+	address.Port = atoi(numtext.c_str());
+
+	mas.push_back(address);
+	intializeFastoreModule(db, mas);
 }
