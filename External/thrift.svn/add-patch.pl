7b38a6fc3444af200e8dade76798235313418181
jklowden <jklowden@b30075cd-70d6-584e-9a73-6433d0c45435>
2012-09-12 19:23:09 +0000

    Fastore builds without warning on VS
    git-svn-id: https://svn.alphora.com:8443/svn/Fastore/trunk@544 b30075cd-70d6-584e-9a73-6433d0c45435

--- External/thrift.svn/add-patch.pl
+++ External/thrift.svn/add-patch.pl
@@ -1,11 +1,33 @@
 # /usr/bin/perl
 #$Id$
 
+use Getopt::Std;
+
+getopts('px', \%opts);
+
+sub up_to_date
+{ my($patch, $src) = @_;
+  my ($patch_mtime, $src_mtime);
+  my ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,
+      $atime,$ctime,$blksize,$blocks);
+  
+  ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,
+   $atime,$patch_mtime,$ctime,$blksize,$blocks)
+      = stat($patch) or die "no such patch: '$patch'\n";
+
+  ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,
+   $atime,$src_mtime,$ctime,$blksize,$blocks)
+      = stat($src) or die "no such source file: '$patch'\n";
+
+  return $src_mtime < $patch_mtime;
+}
+
 $pwd = qx(echo %cd%);
 chomp $pwd;
 $pwd .= '\\patch\\';
 
-@patches = grep { s!\Q$pwd\E!!g; s/\.diff$//; } qx(dir /s/b patch);
+stat "$pwd" or die qq(no such dir "$pwd"\n);
+@patches = grep { s!\Q$pwd\E!!g; s/\.diff$//; } qx(dir /s/b $pwd);
 chomp @patches;
 print "Existing patches:\n\t", join ("\n\t", @patches), $/;
 
@@ -13,22 +35,28 @@ print "Existing patches:\n\t", join ("\n\t", @patches), $/;
 #print join $/, @changes;
 
 Change:
-foreach my $chg (@changes) {
+foreach my $src (@changes) {
     foreach my $p (@patches) {
-	next Change if $chg =~ /\Q$p\E/;
+	if( $src =~ /\Q$p\E/ ) {
+	    next Change if up_to_date( "${pwd}$p.diff", $src );
+	}
     }
-    push @todo, $chg;
+    push @todo, $src;
 }
 
-print "Other modified files:\n\t", 
+print "Files modified since patch was generated:\n\t", 
     join ("\n\t", grep { s!trunk\\lib\\cpp\\src\\thrift\\!! } @todo ), $/;
+
+if( ! $opts{x} ) {
+    exit 0;
+}
 
 for $f (@todo) {
     my @diff = qx(svn diff trunk/lib/cpp/src/thrift/$f);
     die unless @diff;
     my $name = "patch\\$f.diff";
-    print "creating: $name\n";
-    die if -e $name;
+    my $verb = (-e $name)? 'overwriting:' : 'creating:';
+    printf "%-16s $name\n", $verb;
     open PATCH, ">$name" or die "cannot open file '$name': $!\n";
     print PATCH @diff;
     close PATCH;
